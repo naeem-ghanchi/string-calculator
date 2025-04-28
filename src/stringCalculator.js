2d2ea7c08139f586da8293a73b351deaefdbd711
naeem <gnaeem111@gmail.com>
2025-04-28 16:51:02 +0530

    Single number returns the number

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -2,9 +2,12 @@ function add(numbers) {
   if (numbers === "") {
     return 0;
   }
+
+  return parseInt(numbers);
 }
 
 
-console.log(add(""));
+console.log("should return 0 for empty string", add(""));
+console.log("should return number for single number string", add("5"));
 
 module.exports = add;
